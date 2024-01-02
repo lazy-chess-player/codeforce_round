2420fc9a7ae1e7b80268116191d330e413ad986d
lazy-chess-player <129977464+lazy-chess-player@users.noreply.github.com>
2024-01-02 21:07:03 +0800

    24/01/02

---
+++ div2/Round 915/B.cpp
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <list>
+
+#define fi first
+#define se second
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pss> vpss;
+typedef vector<vi> vvi;
+
+typedef queue <int> qi;
+typedef queue <ll> ql;
+typedef queue <pii> qpii;
+typedef queue <psi> qpsi;
+
+typedef priority_queue<int> pqi;
+typedef priority_queue<string> pqs;
+typedef priority_queue<pii> pqpii;
+typedef priority_queue<psi> pqpsi;
+
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<string, int> umapsi;
+
+const int N = 1e5 + 5;
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vvi tree(n + 1);
+
+    int a, b;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    int cnt = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (tree[i].size() == 1) 
+        {
+            cnt++;
+        }
+    }
+    cout << (cnt + 1) / 2 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
